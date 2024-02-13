073a1d0cbc7c134d6b5e15b434f0435d4d4ec4ff
M03V01 <m.vezio@campus.unimib.it>
2024-02-13 13:32:16 +0100

    carousel home nuovo

---
+++ root/js/custom.js
@@ -0,0 +1,47 @@
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slidesLength = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 75}vh`;
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+    const sliderHeight  = sliderContainer.clientHeight;
+    if(direction === 'up'){
+        activeSlideIndex++;
+        if(activeSlideIndex > slidesLength -1){
+            activeSlideIndex = 0;
+        }
+    }else if(direction === 'down'){
+        activeSlideIndex--;
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesLength - 1;
+        }
+    }
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+};
+
+let scrolling = false;
+const scrollDelay = 1000; // Tempo di ritardo tra uno scroll e l'altro in millisecondi
+
+sliderContainer.addEventListener('wheel', (event) => {
+    event.preventDefault(); // Impedisci lo scorrimento della pagina
+    if (!scrolling) {
+        scrolling = true;
+        setTimeout(() => {
+            scrolling = false;
+        }, scrollDelay);
+
+        const deltaY = event.deltaY;
+        const direction = deltaY > 0 ? 'down' : 'up';
+        changeSlide(direction);
+    }
+});
