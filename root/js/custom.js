32a08152ea15cb3ca0e580a6fd6fd91fe815f933
M03V01 <m.vezio@campus.unimib.it>
2024-03-07 11:55:50 +0100

    risoluzioni warning slider home

--- root/js/custom.js
+++ root/js/custom.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sliderContainer = document.querySelector('.slider-container');
     const slideRight = document.querySelector('.right-slide');
     const slideLeft = document.querySelector('.left-slide');
     const upButton = document.querySelector('.up-button');
