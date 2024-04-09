79f4cc3d646f5db87d77dbe13ad00c962a55dc4e
M03V01 <m.vezio@campus.unimib.it>
2024-04-09 19:24:24 +0200

    modifiche custom.js

--- root/js/custom.js
+++ root/js/custom.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let activeSlideIndex = 0;
     let isTransitioning = false;
 
-    // Clona tutte le slide e le posiziona correttamente
+
     for (let i = 0; i < slidesLength; i++) {
         const cloneRight = slideRight.children[i].cloneNode(true);
         const cloneLeft = slideLeft.children[i].cloneNode(true);
@@ -27,24 +27,24 @@ document.addEventListener("DOMContentLoaded", function() {
         if (direction === 'up') {
             activeSlideIndex++;
             if (activeSlideIndex >= slidesLength) {
-                activeSlideIndex = 0; // Torna alla prima slide se si è alla fine
+                activeSlideIndex = 0;
             }
         } else if (direction === 'down') {
             activeSlideIndex--;
             if (activeSlideIndex < 0) {
-                activeSlideIndex = slidesLength - 1; // Torna all'ultima slide se si è all'inizio
+                activeSlideIndex = slidesLength - 1;
             }
         }
 
-        // Rimuovi le transizioni per posizionare le slide correttamente
+
         slideRight.style.transition = 'none';
         slideLeft.style.transition = 'none';
 
-        // Imposta le trasformazioni per posizionare le slide
+
         slideRight.style.transform = `translateY(-${activeSlideIndex * 100}%)`;
         slideLeft.style.transform = `translateY(-${activeSlideIndex * 100}%)`;
 
-        // Ripristina il flag di transizione dopo la fine della transizione
+
         setTimeout(() => {
             isTransitioning = false;
         }, 500);
@@ -56,6 +56,6 @@ document.addEventListener("DOMContentLoaded", function() {
         downButton.addEventListener('click', () => changeSlide('down'));
     }
 
-    // Attivare lo slider per entrambe le modalità (desktop e mobile)
+
     activateSlider();
 });
