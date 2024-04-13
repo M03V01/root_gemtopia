6c28ce5cecb398794bd2232ce46d38e9335562d5
M03V01 <m.vezio@campus.unimib.it>
2024-04-13 10:49:57 +0200

    modifiche finali timeline about e ritocchi home

---
+++ root/js/script._about.js
@@ -0,0 +1,95 @@
+$(function(){
+   $('.timeline-nav-about').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.timeline-slider',
+      centerMode: true,
+      focusOnSelect: true,
+       mobileFirst: true,
+      arrows: false,
+       vertical:true,
+      infinite:true,
+       responsive: [
+           {
+               breakpoint: 1200,
+               settings: {
+                   slidesToShow: 5,
+                   slidesToScroll: 1,
+
+               }
+           },
+           {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+              slidesToScroll: 1,
+
+           }
+          },
+         {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+              vertical:true,
+              adaptiveHeight: true,
+                        }
+                  },
+           {
+               breakpoint: 0,
+               settings: {
+                   slidesToShow: 4,
+                   slidesToScroll: 1,
+                   vertical:true,
+                   adaptiveHeight: true,
+                   centerMode: true,
+                   centerPadding: '-5px',
+               }
+           }
+     ]
+  });
+  
+   $('.timeline-slider-about').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.timeline-nav',
+      centerMode: true,
+       centerPadding: '10px',
+       mobileFirst: true,
+       speed: 500,
+       vertical:true,
+       responsive: [
+           {
+               breakpoint: 992,
+               settings: {
+                   slidesToShow: 3,
+                   slidesToScroll: 1,
+               }
+           },
+           {
+               breakpoint: 768,
+               settings: {
+                   slidesToShow: 3,
+                   slidesToScroll: 1,
+
+
+
+               }
+           },
+         {
+          breakpoint: 0,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              centerMode: true,
+              vertical:true,
+              verticalSwiping:true,
+
+          }
+        },
+
+     ]
+  });
+ 
+});
